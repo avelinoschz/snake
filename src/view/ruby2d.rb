41edc57f8dab174fa396440db8fac9c1dd6e22ba
Avelino Sanchez <avelino.schz@gmail.com>
2022-08-07 02:40:26 -0500

    impl render snake

--- src/view/ruby2d.rb
+++ src/view/ruby2d.rb
@@ -32,6 +32,16 @@ module View
     end
 
     def render_snake(state)
+      extend Ruby2D::DSL
+      snake = state.snake
+      snake.positions.each do |pos|
+        Square.new(
+          x: pos.col * @pixel_size,
+          y: pos.row * @pixel_size,
+          size: @pixel_size,
+          color: "green"
+        )
+      end
     end
   end
 end
